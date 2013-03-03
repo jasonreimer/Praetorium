b087edd073134a83d182042a06d2cd61774c03c8
Jason Reimer <jareimer1@gmail.com>
2013-03-03 15:22:14 -0600

    completed event aggregator tests

--- Source/Praetorium.Tests/Eventing/EventAggregatorTests.cs
+++ Source/Praetorium.Tests/Eventing/EventAggregatorTests.cs
@@ -1,39 +1,67 @@
 ﻿using NUnit.Framework;
 using Praetorium.Eventing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Should;
+using System;
 
 namespace Praetorium.Tests.Eventing
 {
+
     [TestFixture(Category = Categories.Eventing)]
     public class Listener_subscriptions
     {
         [Test]
-        public void via_injetion_should_receive_messages()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
         public void should_receive_message_on_send()
         {
-            Assert.Fail();
+            var listener = new EmptyEventListener();
+            var eventAggregator = new EventAggregator();
+
+            eventAggregator
+                .RegisterListener(listener)
+                .Send<EmptyEvent>();
+
+            listener.EmptyHandleCount.ShouldEqual(1);
         }
 
         [Test]
         public void can_listen_for_many_message_types()
         {
-            Assert.Fail();
+            var listener = new EmptyEventListener();
+            var eventAggregator = new EventAggregator();
+
+            eventAggregator
+                .RegisterListener(listener)
+                .Send("test");
+
+            listener.StringHandleCount.ShouldEqual(1);
         }
 
         [Test]
         public void should_not_receive_messages_after_disposal()
         {
-            Assert.Fail();
+            var listener = new EmptyEventListener();
+            var eventAggregator = new EventAggregator();
+
+            eventAggregator
+                .RegisterListener(listener)
+                .UnregisterListener(listener)
+                .Send("test");
+
+            listener.StringHandleCount.ShouldEqual(0);
+        }
+    }
+
+    [TestFixture(Category = Categories.Eventing)]
+    public class Injected_listeners
+    {
+        [Test]
+        public void should_receive_messages()
+        {
+            var listener = new EmptyEventListener();
+            var eventAggregator = new EventAggregator(new IListener[] { listener });
+
+            eventAggregator.Send<EmptyEvent>();
+
+            listener.EmptyHandleCount.ShouldEqual(1);
         }
     }
 
@@ -60,7 +88,7 @@ namespace Praetorium.Tests.Eventing
         public void should_receive_message_on_send()
         {
             int number = 0;
-            var eventAggregator = new EventAggregator(null);
+            var eventAggregator = new EventAggregator();
             
             eventAggregator.SubscribeTo<EmptyEvent>().SendTo(e => number++);
             eventAggregator.Send<EmptyEvent>();
@@ -70,4 +98,35 @@ namespace Praetorium.Tests.Eventing
     }
 
     public class EmptyEvent { }
+
+    public class EmptyEventListener : IListener<EmptyEvent>, IListener<string>
+    {
+        public int EmptyHandleCount { get; private set; }
+        public int StringHandleCount { get; private set; }
+
+        public void Handle(EmptyEvent message)
+        {
+            EmptyHandleCount++;
+        }
+
+        public void Handle(string message)
+        {
+            StringHandleCount++;
+        }
+    }
+
+    public class ActionEventListener<TEvent> : IListener<TEvent> where TEvent : class
+    {
+        private readonly Action<TEvent> _action;
+
+        public ActionEventListener(Action<TEvent> action)
+        {
+            _action = action;
+        }
+
+        public void Handle(TEvent message)
+        {
+            _action(message);
+        }
+    }
 }
