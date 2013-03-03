1b0a6899ad3591cb1250aed0f23406c3a053191a
Jason Reimer <jareimer1@gmail.com>
2013-03-03 11:00:58 -0600

    added SynchronizedEventAggregator

---
+++ Source/Praetorium/Eventing/SynchronizedEventAggregator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Praetorium.Eventing
+{
+    public class SynchronizedEventAggregator : EventAggregator
+    {
+        private readonly SynchronizationContext _syncContext;
+
+        public SynchronizedEventAggregator(SynchronizationContext syncContext, IListener[] listeners)
+            : base(listeners)
+        {
+            Ensure.ArgumentNotNull(() => syncContext, ref _syncContext);
+        }
+
+        protected override void SendToEach<TEvent>(TEvent @event, IEnumerable<IListener<TEvent>> listeners)
+        {
+            _syncContext.Send(_ => base.SendToEach<TEvent>(@event, listeners), null);
+        }
+    }
+}
