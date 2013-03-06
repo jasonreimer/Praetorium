1aff08dee782e250c637f6e3f8b38c26343e935e
Jason Reimer <jareimer1@gmail.com>
2013-03-06 07:44:12 -0600

    updated assembly info for SL build

--- Source/Praetorium (SL5)/Properties/AssemblyInfo.cs
+++ Source/Praetorium (SL5)/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Praetorium__SL5_")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Praetorium - SL5")]
+[assembly: AssemblyDescription("Silverlight 5 version assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Praetorium__SL5_")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praetorium")]
+[assembly: AssemblyCopyright("Copyright © Jason Reimer 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("035bad7b-c062-4489-bcb6-fc96d3c406f0")]
+[assembly: Guid("31acfe34-f648-4af7-9a5c-2b372b57200f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +28,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
