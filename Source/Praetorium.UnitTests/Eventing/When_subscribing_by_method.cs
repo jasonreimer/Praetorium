05c7e3e5e7e7bd34fd4788b68346a33af12c75fa
Jason Reimer <jareimer1@gmail.com>
2013-09-01 18:13:23 -0500

    added test to cover method based subscriptions

---
+++ Source/Praetorium.UnitTests/Eventing/When_subscribing_by_method.cs
@@ -0,0 +1,37 @@
+﻿using Machine.Specifications;
+using Praetorium.Eventing;
+
+namespace Praetorium.UnitTests.Eventing
+{
+    class When_subscribing_by_method
+    {
+        static SampleListener Listener;
+        static EventAggregator Aggregator;
+
+        Establish context = () =>
+        {
+            Listener = new SampleListener();
+            Aggregator = new EventAggregator();
+        };
+
+        Because of = () =>
+        {
+            Aggregator.SubscribeTo<NulloEvent>()
+                      .SendTo(Listener.Handle);
+            Aggregator.Send<NulloEvent>();
+        };
+
+        It should_receive_the_message_on_send = () =>
+            Listener.CallCount.ShouldEqual(1);
+    }
+
+    class SampleListener
+    {
+        public int CallCount { get; set; }
+
+        public void Handle(NulloEvent e)
+        {
+            CallCount++;
+        }
+    }
+}
