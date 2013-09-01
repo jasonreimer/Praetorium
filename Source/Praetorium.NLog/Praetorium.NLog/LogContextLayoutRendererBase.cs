8739f71c0bc41af3b1d5a3d2a42fb0c5f71833f1
Jason Reimer <jareimer1@gmail.com>
2013-08-31 22:30:43 -0500

    removed ILogContextScope

--- Source/Praetorium.NLog/Praetorium.NLog/LogContextLayoutRendererBase.cs
+++ Source/Praetorium.NLog/Praetorium.NLog/LogContextLayoutRendererBase.cs
@@ -2,30 +2,22 @@
 using NLog.Config;
 using NLog.LayoutRenderers;
 using Praetorium.Contexts;
-using Praetorium.Logging;
 using System.Text;
 
 namespace Praetorium.NLog
 {
     public abstract class LogContextLayoutRendererBase : LayoutRenderer
     {
-        private static readonly string _contextKey = typeof(ILogContextScope).FullName;
-
         [RequiredParameter]
         [DefaultParameter]
         public string Item { get; set; }
 
         protected override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
-            var context = GetContext().GetOrDefault<ILogContextScope>(_contextKey);
+            var context = GetContext();
 
-            if (context != null)
-            {
-                var item = context[Item];
-
-                if (item != null)
-                    builder.Append(item);
-            }
+            if (context.Contains(Item))
+                builder.Append(context[Item]);
         }
 
         protected abstract IContext GetContext();
